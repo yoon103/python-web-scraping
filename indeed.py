b90ceecc91ceca81a95ae78b9655f271198b338c
Yoonju An <anyj103@gmail.com>
2020-04-18 15:24:11 +0900

    Extract Indeed titles

--- indeed.py
+++ indeed.py
@@ -21,7 +21,11 @@ def extract_indeed_pages():
 
 def extract_indeed_jobs(last_page):
     jobs = []
-    for page in range(last_page):
-        result = requests.get(f"{URL}&start={page*LIMIT}")
-        print(result.status_code)
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, 'html.parser')
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+        title = result.find("h2", {"class": "title"}).find("a")["title"]
+        print(title)
     return jobs
